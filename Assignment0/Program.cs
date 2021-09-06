304c3b7483d79c3553cdfcd13183dd9cff342736
Lasse Faurby <lasse_fk@hotmail.com>
2021-09-06 16:46:21 +0200

    new print

--- Assignment0/Program.cs
+++ Assignment0/Program.cs
@@ -6,7 +6,7 @@ namespace Assignment0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Thor og Flyv!");
         }
     }
 }
